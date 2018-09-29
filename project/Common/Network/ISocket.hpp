41fd9b0aa0b96c805323213993e2d2f59309b72f
Viguier Benjamin <benjamin.viguier@epitech.eu>
2018-09-29 11:17:03 +0200

    update i socket

--- project/Common/Network/ISocket.hpp
+++ project/Common/Network/ISocket.hpp
@@ -15,6 +15,15 @@
 namespace nw {
 
 class ISocket {
+public:
+	ISocket() = default;
+
+	virtual std::size_t	send(std::uint8_t *buff, std::size_t len) = 0;
+	virtual std::size_t	receive(std::uint8_t *buff, std::size_t len) = 0;
+	virtual std::size_t	available(void) const = 0;
+};
+
+class ASocket: public ISocket {
 protected:
 	enum class Type {
 		UDP,
@@ -23,37 +32,36 @@ protected:
 public:
 	using Handler = std::function<int(std::size_t)>;
 	
-	ISocket(Type connectionType): _type(connectionType) {}
-
-	virtual std::size_t	send(std::uint8_t *buff, std::size_t len) = 0;
-	virtual std::size_t	receive(std::uint8_t *buff, std::size_t len) = 0;
-	virtual std::size_t	available(void) const = 0;
+	ASocket(Type connectionType): _type(connectionType) {}
 	inline std::function<void()>
-				addHandlerOnReadable(typename ::nw::ISocket::Handler &&func);
+				addHandlerOnReadable(typename ASocket::Handler &&func);
 	
-	std::list<Handler>	_hdls;
 protected:
 	inline Type	_getType(void) const { return _type; }
 
+	std::list<Handler>	_hdls;
 	Type			_type;
 };
 
+class ATCPSocket: public ASocket {
+public:
+	ATCPSocket(): ASocket(Type::TCP) {}
+
+	virtual void	connect(std::string const &host, std::uint16_t port) = 0;
+	inline void	setOnDisconnect(std::function<void(void)> const &hdl) { _onDisconnect = hdl; }
+protected:
+	using		ASocket::_getType;
+	using		ASocket::_hdls;
+	std::function<void(void)>
+			_onDisconnect;
+};
+
 std::function<void()>
-ISocket::addHandlerOnReadable(Handler &&func) {
+ASocket::addHandlerOnReadable(Handler &&func) {
 	auto	it = _hdls.insert(_hdls.end(), func);
 
 	return [this, it] {
 		_hdls.erase(it);
 	};
 }
-
-class ITCPSocket: public ISocket {
-public:
-	ITCPSocket(): ISocket(Type::TCP) {}
-
-	virtual void	connect(std::string const &host, std::uint16_t port) = 0;
-protected:
-	Type			_type;
-};
-
 }
