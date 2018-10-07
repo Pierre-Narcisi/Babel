05fa744ba9c99bd7b4acf0d98f54ca37bebbdbae
Viguier Benjamin <benjamin.viguier@epitech.eu>
2018-10-07 20:41:07 +0200

    push

--- Client/call.cpp
+++ Client/call.cpp
@@ -23,7 +23,13 @@ void call::process()
 	SoundWrapper &soundWrapper = Singletons::getSoundWrapper();
 	
 	connect(&_t, &QTimer::timeout, [this, &soundWrapper] {
-		
+		soundWrapper.getPa().record();
+		CompData d = soundWrapper.getPa().getData();
+
+		auto	p = babel::protocol::VoicePacket::create(d.data.size());
+		std::memmove(p->data, d.data.data(), p->size);
+		p->length = d.length;
+		_udpWrapper->sendData(*p, _ip);
 	});
 	_t.start(10);
 }
